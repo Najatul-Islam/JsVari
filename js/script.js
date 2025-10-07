2c0f22fd6051edf5b50020941a8c63f1dc503c23
Sadika <najatulislam111@gmail.com>
2025-10-07 21:07:22 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+alert ("connected Done")
+
+console.log("Welcome to JavaScript");
+
+
+var x = 5;
+var y = 10;
+var z = x + y;
+console.log(z);
+
+const g = 9.8;
+console.log(g);
+
+let product = 10;
+console.log(product+" "+"/"+ " "+2+" "+"="+" "+5);
+
+let age =15;
+ if (age < 18) {
+    console.log("You are not eligible to vote");
+ }
+ else{
+    console.log("You are eligible to vote");
+ }
